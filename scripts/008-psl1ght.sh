07cb8d2f503e85a2615b46b462f4472d4e00691a
blckbearx <nachogsr@gmail.com>
2026-04-27 10:41:19 -0300

    Point to own PSL1GHT repo.

--- scripts/008-psl1ght.sh
+++ scripts/008-psl1ght.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -e
 # psl1ght.sh by Naomi Peori (naomi@peori.ca)
 
-PSL1GHT_VERSION=${PSL1GHT_VERSION:="a4631bc16788741a013c861746e87d532e1c6dbf"}
+PSL1GHT_VERSION=${PSL1GHT_VERSION:="38f879b12e7a79a1316e1986291848b2dd1deb3e"}
 
 ## Download the source code.
-wget "https://github.com/ps3aqua/PSL1GHT/archive/${PSL1GHT_VERSION}.tar.gz" -O psl1ght-${PSL1GHT_VERSION}.tar.gz
+wget "https://github.com/blckbearx/PSL1GHT/archive/${PSL1GHT_VERSION}.tar.gz" -O psl1ght-${PSL1GHT_VERSION}.tar.gz
 
 ## Unpack the source code.
 rm -Rf psl1ght && mkdir psl1ght && tar --strip-components=1 --directory=psl1ght -xzf psl1ght-${PSL1GHT_VERSION}.tar.gz
